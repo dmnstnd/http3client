3777e52a7337c50faae5d998ae1ee00f771c68bc
dmnstnd <136969916+dmnstnd@users.noreply.github.com>
2023-06-18 22:22:48 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(
-    name="dmnstnd_http3_client",
+    name="http3_client",
     version="1.0.0",
     install_requires=["requests"],
     packages=setuptools.find_packages(),
